a1f8fd45452be4182779aa70cff0f3b3bf0bbf92
asczyk <aron.scheffczyk@gmail.com>
2025-03-18 13:39:53 +0100

    add appointment

---
+++ src/components/diagnostic/calender-embedding.tsx
@@ -0,0 +1,13 @@
+export default function CalenderEmbedding() {
+  return (
+    <div>
+      <iframe
+        src="https://app.acuityscheduling.com/schedule.php?owner=34571969&calendarID=11520693&ref=embedded_csp"
+        title="Schedule Appointment"
+        width="100%"
+        height="400"
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+}
