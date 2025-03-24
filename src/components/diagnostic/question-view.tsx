a69fdcbf348f18f9443fee97058c5432d22add19
asczyk <aron.scheffczyk@gmail.com>
2025-03-24 09:37:38 +0100

    remove red border

--- src/components/diagnostic/question-view.tsx
+++ src/components/diagnostic/question-view.tsx
@@ -177,7 +177,7 @@ export default function QuestionView() {
                         key={option.value}
                         className={`border rounded-lg p-4 transition-colors ${
                           selectedOption === option.value
-                            ? "border-[#DB5461] bg-pink-50"
+                            ? "border-gray-300 bg-gray-50"
                             : "border-gray-200 hover:border-gray-300"
                         }`}
                       >
