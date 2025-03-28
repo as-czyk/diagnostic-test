2a75c29e6d6e352dfa8df48b64af34a6d97d7fce
asczyk <aron.scheffczyk@gmail.com>
2025-03-28 10:16:48 +0100

    prepare for personalized study plan

--- app/f/generate-result/page.tsx
+++ app/f/generate-result/page.tsx
@@ -1,22 +1,8 @@
-"use client";
-
-import LoaderPage from "@/components/diagnostic/loading-page";
 import { Routes } from "@/routes/Routes";
 import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function GenerateResult() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  if (loading) {
-    return <LoaderPage />;
-  }
+  //Logic to generate personalized Study Plan
 
   return redirect(Routes.BookingPage);
 }
