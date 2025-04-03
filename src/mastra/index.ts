ea0077f23eaa8679d14d4bab0f402c2e3c915241
asczyk <aron.scheffczyk@gmail.com>
2025-04-03 08:31:51 +0200

    vercel deployer

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,8 +2,16 @@ import { Mastra } from "@mastra/core/mastra";
 import { createLogger } from "@mastra/core/logger";
 import { SatStudyPlanAgent } from "./agents";
 import { StudyPlanWorkflow } from "./workflows";
+import { VercelDeployer } from "@mastra/deployer-vercel";
+
+const deployer = new VercelDeployer({
+  teamSlug: "aron-scheffczyks-projects",
+  projectName: "diagnostic-test",
+  token: process.env.MASTRA_VERCEL_TOKEN!,
+});
 
 export const mastra = new Mastra({
+  deployer: deployer,
   agents: { SatStudyPlanAgent },
   workflows: { StudyPlanWorkflow },
   logger: createLogger({
